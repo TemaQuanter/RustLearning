0f2dd8377e0c7c01d879c0bb5d4001aef50284f8
Artem Mikheev <artemmikheev@Artems-MacBook-Pro.local>
2023-04-06 06:22:00 +0400

    chapter 5 done

--- chapter5/program2/src/main.rs
+++ chapter5/program2/src/main.rs
@@ -25,6 +25,16 @@ struct Triangle {
 // Implementation for Triangle structure.
 //
 impl Triangle {
+    // This function initializes a Triangle.
+    //
+    fn new(side1: f32, side2: f32, side3: f32) -> Self {
+        Self {
+            side1,
+            side2,
+            side3,
+        }
+    } // end init()
+
     // This function sets a Triangle.
     //
     fn set(&mut self, side1: f32, side2: f32, side3: f32) {
@@ -56,6 +66,7 @@ fn main() {
 
     let tr1: Triangle;
     let mut tr2: Triangle;
+    let tr3: Triangle;
 
     // Initialize Triangles.
 
@@ -71,6 +82,8 @@ fn main() {
         side3: 0.0,
     }; // end Triangle
 
+    tr3 = Triangle::new(32.0, 45.0, 27.0);
+
     tr2.set(11.0, 12.0, 7.0);
 
     // Display some information about Triangles.
@@ -79,7 +92,15 @@ fn main() {
     println!("Some information about this triangle");
     println!("Perimeter: {}\nArea: {}", tr1.perimeter(), tr1.area());
 
+    println!();
+
     println!("Triangle tr2: {:#?}", tr2);
     println!("Some information about this triangle");
     println!("Perimeter: {}\nArea: {}", tr2.perimeter(), tr2.area());
+
+    println!();
+
+    println!("Triangle tr3: {:#?}", tr3);
+    println!("Some information about this triangle");
+    println!("Perimeter: {}\nArea: {}", tr3.perimeter(), tr3.area());
 } // end main()
