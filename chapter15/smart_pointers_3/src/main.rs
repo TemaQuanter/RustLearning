aa6fec10e2877023f91e8206843b8d2376e38d02
Artem Mikheev <artemmikheev@Artems-MacBook-Pro.local>
2023-04-13 17:10:39 +0400

    smart pointers learnt

---
+++ chapter15/smart_pointers_3/src/main.rs
@@ -0,0 +1,45 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn main() {
+    prog1();
+    prog2();
+    prog3();
+} // end main()
+
+fn prog1() {
+    let x: Box<String> = Box::new("Hello, world!".to_string());
+
+    let a: Box<String> = x;
+    // This will cause an error!
+    // let b: Box<String> = x;
+} // end prog1()
+
+fn prog2() {
+    let x: Rc<String> = Rc::new("Hello, world!".to_string());
+
+    let a: Rc<String> = Rc::clone(&x);
+    println!("Count: {}", Rc::strong_count(&x));
+    let b: Rc<String> = Rc::clone(&x);
+    println!("Count: {}", Rc::strong_count(&x));
+    {
+        let c: Rc<String> = Rc::clone(&x);
+        println!("Count: {}", Rc::strong_count(&x));
+    }
+    println!("Count: {}", Rc::strong_count(&x));
+    println!("a = {}\nb = {}", a, b);
+} // end prog2()
+
+fn prog3() {
+    let x: RefCell<Rc<String>> = RefCell::new(Rc::new(String::new()));
+
+    assign(&x);
+
+    println!("{}", *x.borrow());
+}
+
+fn assign(rf: &RefCell<Rc<String>>) {
+    let st: Rc<String> = Rc::new(String::from("Hello, world!"));
+
+    *rf.borrow_mut() = Rc::clone(&st);
+}
