672bcfab4de35f78182572ba30231c9134893be2
Artem Mikheev <artemmikheev@Artems-MacBook-Pro.local>
2023-04-18 18:40:15 +0100

    single-threaded server is finished

--- chapter20/hello/src/main.rs
+++ chapter20/hello/src/main.rs
@@ -17,19 +17,21 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader: BufReader<&mut TcpStream> = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line: String = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line: &str = "HTTP/1.1 200 OK";
-    let content: String = fs::read_to_string("index.html").expect("Failed to read the HTML page");
+    // Check what type of request it is and act accordingly.
+
+    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+        // It is a GET request
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    }; // end if
+
+    let content: String = fs::read_to_string(file_name).expect("Failed to read an HTML page");
     let length: usize = content.len();
 
     let response: String = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
 
     stream.write_all(response.as_bytes()).unwrap();
-
-    println!("Request: {:#?}", http_request);
 } // end handle_connection()
